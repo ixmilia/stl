81ad5cbf28e27cb74105c502248412700c1cb211
Brett V. Forsgren <brett.forsgren@outlook.com>
2016-06-01 12:45:45 -0700

    properly create a multi-targeted package with GitLink support

--- src/IxMilia.Stl/Properties/AssemblyInfo.cs
+++ src/IxMilia.Stl/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Reflection;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("IxMilia")]
 [assembly: AssemblyProduct("IxMilia.Stl")]
-[assembly: AssemblyCopyright("Copyright © IxMilia 2015")]
+[assembly: AssemblyCopyright("Copyright © IxMilia 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -24,5 +24,6 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.1.1.0")]
+[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyInformationalVersion("0.1.1.0")]
