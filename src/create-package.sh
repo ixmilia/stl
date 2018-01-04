e1750a289a822065ee18779dc0df54db0b03cf35
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-01-03 16:00:43 -0800

    tag official binaries with git SHA

--- src/create-package.sh
+++ src/create-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
-PROJECT=./IxMilia.Stl/IxMilia.Stl.csproj
+_SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
+PROJECT=$_SCRIPT_DIR/IxMilia.Stl/IxMilia.Stl.csproj
 dotnet restore $PROJECT
-dotnet pack --include-symbols --include-source --configuration Release $PROJECT
-
+dotnet pack --include-symbols --include-source --configuration Release $PROJECT /p:OfficialBuild=true
